2b8a7893ced4d9d63a2e37ffa3f475d036e898c7
whyisthistheway <ouro.tournament@gmail.com>
2023-12-07 08:38:52 +0100

    har oppdatert get request med buttons som skifter innhold vet ikke om jeg gjorde det på riktig måte men det får jeg se senere

--- nodeapplications/getrequest/src/home.js
+++ nodeapplications/getrequest/src/home.js
@@ -1,18 +1,28 @@
-import './App.css';
-export default function Home(){
-   return(
+import React, { useState } from 'react';
+import './App.css'; // Assuming you have a CSS file for styling
+
+export default function Home() {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonType) => {
+    setActiveButton(buttonType);
+  };
+
+  return (
     <div>
-    <div className="buttonContainer">
-   <button className='button'>Select</button>
-   <button className='button'>Update</button>
-   <button className='button'>Insert</button>
-   <button className='button'>Delete</button>
-   </div>
-   <div className='requestContainer'>
-    <h1>Get-request</h1>
-    <h1>Put-request</h1>
-    <h1>Post-request</h1>
-   </div>
-   </div>
-   ) 
+      <div className="buttonContainer">
+        <button className='button' onClick={() => handleButtonClick('select')}>Select</button>
+        <button className='button' onClick={() => handleButtonClick('update')}>Update</button>
+        <button className='button' onClick={() => handleButtonClick('insert')}>Insert</button>
+        <button className='button' onClick={() => handleButtonClick('delete')}>Delete</button>
+      </div>
+
+      <div className='requestContainer'>
+        {activeButton === 'select' && <h1>Get-request</h1>}
+        {activeButton === 'update' && <h1>Put-request</h1>}
+        {activeButton === 'insert' && <h1>Post-request</h1>}
+        {activeButton === 'delete' && <h1>Delete-request</h1>}
+      </div>
+    </div>
+  );
 }
